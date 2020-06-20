cd0ff792d1cabde970bc70f7898d9b9e76f17343
olabode-dev <muhbrahc1@gmail.com>
2020-06-20 11:30:45 +0100

    added the nav bars

---
+++ 3/navbar.js
@@ -0,0 +1,26 @@
+// $(document).ready(function() {
+//     $("button").click(function () {
+//         if ($("button").text == "☰") {
+//             $("button").text("🞬");
+//         }
+//         else {
+//             $("button").text("☰");
+//         }
+//         $("li").toggle("slow");
+//     });
+// });
+$(document).ready(function(){
+    /* function called when you click of the button */
+    $("button").click(function(){
+        
+        /* this if else to change the text in the button */
+        if($("button").text() == "☰"){
+         $("button").text("🞬");
+        }else{
+          $("button").text("☰");
+        }
+      
+      /* this function toggle the visibility of our "li" elements */
+      $("li").toggle("slow");
+    });
+  });
